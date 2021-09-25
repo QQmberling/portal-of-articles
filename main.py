dd76ec03a54b00e9ed1c55196e531d696f7c53bb
QQmberling <67502319+QQmberling@users.noreply.github.com>
2021-09-25 13:11:08 +0300

    another try fix databases on heroku

--- main.py
+++ main.py
@@ -15,31 +15,29 @@ if __name__ == '__main__':
     with open('text.txt') as file:
         text = file.read()
 
-    drop_db()
-    create_db()
+    if create_db():
+        user = User(login='admin', password=generate_password_hash('admin'), date=datetime.now(TIMEZONE))
+        db.session.add(user)
+        db.session.commit()
 
-    user = User(login='admin', password=generate_password_hash('admin'), date=datetime.now(TIMEZONE))
-    db.session.add(user)
-    db.session.commit()
+        user_info = UserInfo(id=user.id, first_name='Igor', last_name='Pershin', about='I AM NUMBER ONE')
+        db.session.add(user_info)
+        db.session.commit()
 
-    user_info = UserInfo(id=user.id, first_name='Igor', last_name='Pershin', about='I AM NUMBER ONE')
-    db.session.add(user_info)
-    db.session.commit()
+        article = Article(title='Standart Title', intro='Pretty Standart Intro', date=datetime.now(TIMEZONE), author_id=1, text=text)
+        db.session.add(article)
+        db.session.commit()
 
-    article = Article(title='Standart Title', intro='Pretty Standart Intro', date=datetime.now(TIMEZONE), author_id=1, text=text)
-    db.session.add(article)
-    db.session.commit()
+        user = User(login='not admin', password=generate_password_hash('admin'), date=datetime.now(TIMEZONE))
+        db.session.add(user)
+        db.session.commit()
 
-    user = User(login='not admin', password=generate_password_hash('admin'), date=datetime.now(TIMEZONE))
-    db.session.add(user)
-    db.session.commit()
+        user_info = UserInfo(id=user.id, first_name='not Igor', last_name='not Pershin', about='I AM NUMBER TWO')
+        db.session.add(user_info)
+        db.session.commit()
 
-    user_info = UserInfo(id=user.id, first_name='not Igor', last_name='not Pershin', about='I AM NUMBER TWO')
-    db.session.add(user_info)
-    db.session.commit()
-
-    article = Article(title='Second Title', intro='Second Standart Intro', date=datetime.now(TIMEZONE), author_id=2, text=text, able_to_comment=False)
-    db.session.add(article)
-    db.session.commit()
+        article = Article(title='Second Title', intro='Second Standart Intro', date=datetime.now(TIMEZONE), author_id=2, text=text, able_to_comment=False)
+        db.session.add(article)
+        db.session.commit()
 
     app.run()
