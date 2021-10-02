3c0208bd6aa1e71ff481093affba19f1241a8185
QQmberling <67502319+QQmberling@users.noreply.github.com>
2021-10-02 20:07:52 +0300

    change blog.py

--- blog.py
+++ blog.py
@@ -3,7 +3,9 @@ from app import create_app
 from app.database import create_db
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-app.app_context().push()
-create_db()
-app.run()
+if __name__ == '__main__':
+
+    APP = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    with APP.app_context():
+        create_db()
+        APP.run()
