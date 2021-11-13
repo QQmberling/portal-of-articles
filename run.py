b24e77f34d33729f7658b821aef2c2801e5b4a2a
QQmberling <67502319+QQmberling@users.noreply.github.com>
2021-11-13 18:13:44 +0300

    test migrations

--- run.py
+++ run.py
@@ -1,19 +1,17 @@
 import os
 
+from flask_migrate import Migrate
 from app import create_app, db
-from app.database import create_db
 from app.models import User, UserInfo, Comment, Article
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, UserInfo=UserInfo, Article=Article, Comment=Comment)
 
-
-with app.app_context():
-    create_db()
 
 if __name__ == '__main__':
     app.run()
